8135d7aebb0b5862d1613539e53589ef96e83a9f
Miguel Lopez <ing.miguellv@gmail.com>
2023-10-14 12:07:29 -0600

    add appbar and dropdown menu separately from main

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
 import flet as ft
 
+
+from widgets.navbar import navbar
+from widgets.dropdown_transportista import menu_navbar
+
+
 def main(page: ft.Page):
     page.title = "Mailer App"
     page.window_width = 500
+    page.window_height = 650
+    # page.theme_mode = "LIGHT"
 
-    lbl_transportista = ft.Text(value="Transportista:", size=30)
+    page.appbar = navbar
 
-    dd_transportista = ft.Dropdown(
-        label="Transportista",
-        hint_text="Selecciona una opción",
-        autofocus=True,
-        border_width=0.7,
-        options=[
-            ft.dropdown.Option("Transportista 1"),
-            ft.dropdown.Option("Transportista 2"),
-            ft.dropdown.Option("Transportista 3")
-        ]
-    )
+    dd_transportista = menu_navbar
 
     lbl_correos_transportista = ft.Text(value="Destinatarios:", size=30)
 
     container_correos_transportista = ft.Container(
-        # content=[], Acá va la lista de correos
+        # content=, Acá va la lista de correos
         height=250,
         bgcolor="gray",
-        border=ft.border.all(width=0.7, color=ft.colors.INDIGO_400)
+        border=ft.border.all(width=0.7, color=ft.colors.LIGHT_BLUE_400)
     )
 
-    btn_enviar_email = ft.ElevatedButton(text="Enviar email", icon="send_rounded")
+    btn_enviar_email = ft.ElevatedButton( #needs fix the style :|
+        text="Enviar email",
+        icon="send_rounded",
+        color=ft.colors.BLACK87,
+        bgcolor=ft.colors.LIGHT_BLUE_300
+    )
+
+    container_send_email_button = ft.Container(
+        content=btn_enviar_email,
+        height=150,
+        alignment=ft.alignment.bottom_right
+    )
 
     page.add(
-        lbl_transportista,
         dd_transportista,
         lbl_correos_transportista,
         container_correos_transportista,
-        btn_enviar_email
+        container_send_email_button
     )
 
 
---
+++ widgets/dropdown_transportista.py
@@ -0,0 +1,16 @@
+from flet import Dropdown, TextStyle, colors, dropdown
+
+
+menu_navbar = Dropdown(
+        label="Transportista",
+        hint_text="Selecciona una opción",
+        label_style=TextStyle(color=colors.LIGHT_BLUE_400),
+        # autofocus=True,
+        border_width=0.7,
+        border_color=colors.LIGHT_BLUE_400,
+        options=[
+            dropdown.Option(""),
+            dropdown.Option("Transportista 2"),
+            dropdown.Option("Transportista 3")
+        ]
+    )
