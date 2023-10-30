131140a96469bed24932ed82613ce704fd5e3219
Miguel Lopez <ing.miguellv@gmail.com>
2023-10-30 15:30:01 -0600

    agrego API para crear nueva razzon social

--- pages/nuevo.py
+++ pages/nuevo.py
@@ -1,10 +1,78 @@
 import flet as ft
+import requests
+import json
 
 
-from widgets.container_add_button import container_add_transportista
+URL = "https://mrjuw5qxmbxnnsdljvsqfoyv640yondi.lambda-url.us-east-2.on.aws"
+
+
+razon_social = ft.TextField(
+    label="Razón social",
+    hint_text="Agrega un valor",
+    label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
+    border_width=0.7,
+    border_color=ft.colors.LIGHT_BLUE_400
+)
+
+correos = ft.TextField(
+    label="Correos",
+    hint_text="Agrega los correos del transportista",
+    label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
+    border_width=0.7,
+    border_color=ft.colors.LIGHT_BLUE_400,
+    multiline=True,
+    max_lines=9,
+    height=250
+)
 
 
 def _view_():
+    def create_sociedad(e):
+        if razon_social.value != "" and correos.value != "":
+            try:
+                sociedad = {
+                    "nombre": razon_social.value,
+                    "correos": correos.value
+                }
+                sociedad = json.dumps(sociedad)
+
+                response = requests.post(
+                    url=f"{URL}/sociedades",
+                    data=sociedad
+                )
+
+                e.page.snack_bar = ft.SnackBar(
+                    content=ft.Text(
+                        value="Nueva razón social agregada",
+                        text_align=ft.TextAlign.CENTER,
+                    ),
+                    bgcolor=ft.colors.LIGHT_BLUE_200
+                )
+                e.page.snack_bar.duration = 3000
+                e.page.snack_bar.open = True
+
+                razon_social.value = ""
+                correos.value = ""
+                e.page.update()
+
+            except:
+                e.page.snack_bar = ft.SnackBar(
+                    content=ft.Text("Error al insertar a base de datos", text_align=ft.TextAlign.CENTER),
+                    bgcolor="red"
+                )
+                e.page.snack_bar.duration = 3000
+                e.page.snack_bar.open = True
+                e.page.update()
+        else:
+            e.page.snack_bar = ft.SnackBar(
+                content=ft.Text("Todos los campos son obligatorios", text_align=ft.TextAlign.CENTER, size=18),
+                bgcolor="#ff6347"
+            )
+            e.page.snack_bar.duration = 3000
+            e.page.snack_bar.open = True
+            e.page.update()
+
+
 
     return ft.View(
         "/nuevo",
@@ -25,23 +93,16 @@ def _view_():
             ]
         ),
         controls=[
-            ft.TextField(
-                label="Razón social",
-                hint_text="Agrega un valor",
-                label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
-                border_width=0.7,
-                border_color=ft.colors.LIGHT_BLUE_400
-            ),
-            ft.TextField(
-                label="Correos",
-                hint_text="Agrega los correos del transportista",
-                label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
-                border_width=0.7,
-                border_color=ft.colors.LIGHT_BLUE_400,
-                multiline=True,
-                max_lines=9,
-                height=250
-            ),
-            container_add_transportista
+            razon_social,
+            correos,
+            ft.Container(
+                content=ft.ElevatedButton(
+                    text="Agregar",
+                    icon="ADD_CIRCLE_ROUNDED",
+                    on_click=create_sociedad
+                ),
+                height=150,
+                alignment=ft.alignment.bottom_right,
+            )
         ]
     )
