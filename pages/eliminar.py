a8836ba59c3fe6d91645b761f5d57f514441e912
Miguel Lopez <ing.miguellv@gmail.com>
2023-10-31 11:59:32 -0600

    agrego funcionalidad para eliminar razon social y quito widget independiente

--- pages/eliminar.py
+++ pages/eliminar.py
@@ -1,25 +1,110 @@
 import flet as ft
+import requests
 
 
-from widgets.container_delete_button import container_delete_button
+URL = "https://mrjuw5qxmbxnnsdljvsqfoyv640yondi.lambda-url.us-east-2.on.aws"
+
+razon_social = ft.TextField(
+    label="Razón social",
+    hint_text="Buscar transportista",
+    label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
+    border_width=0.7,
+    border_color=ft.colors.LIGHT_BLUE_400,
+    width=420
+)
+
+correos = ft.TextField(
+    label="Correos",
+    label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
+    border_width=0.7,
+    border_color=ft.colors.LIGHT_BLUE_400,
+    multiline=True,
+    max_lines=9,
+    height=250,
+    disabled=True
+)
+
+id_razon_social = ""
 
 
 def _view_():
+    def search_razon_social(e):
+        global id_razon_social
+        nombre = razon_social.value
+        response = requests.get(f"{URL}/sociedades/{nombre}")
 
-    row_search_transportista = ft.Row(
+        if response.status_code == 200:
+            data = response.json()
+            id_razon_social = data["id"]
+            correos.value = data["correos"]
+        else:
+            e.page.snack_bar = ft.SnackBar(
+                content=ft.Text("No se encontró la razón social", text_align=ft.TextAlign.CENTER),
+                bgcolor="#ff6347"
+            )
+            e.page.snack_bar.duration = 3000
+            e.page.snack_bar.open = True
+        
+        e.page.update()
+
+    
+    def delete_razon_social(e):
+        if razon_social.value != "" and id_razon_social != "":
+            try:
+                response = requests.delete(f"{URL}/sociedades/{id_razon_social}")
+
+                if response.status_code == 204:
+                    e.page.snack_bar = ft.SnackBar(
+                        content=ft.Text(
+                            value="Razón social eliminada",
+                            text_align=ft.TextAlign.CENTER,
+                        ),
+                        bgcolor=ft.colors.LIGHT_BLUE_200
+                    )
+                    e.page.snack_bar.duration = 3000
+                    e.page.snack_bar.open = True
+
+                    razon_social.value = ""
+                    correos.value = ""
+
+                if response.status_code == 404:
+                    e.page.snack_bar = ft.SnackBar(
+                        content=ft.Text(
+                            value="No existe la razón social",
+                            text_align=ft.TextAlign.CENTER,
+                        ),
+                        bgcolor="#ff6347"
+                    )
+                    e.page.snack_bar.duration = 3000
+                    e.page.snack_bar.open = True
+
+                e.page.update()
+            except:
+                e.page.snack_bar = ft.SnackBar(
+                    content=ft.Text("Error al insertar a base de datos", text_align=ft.TextAlign.CENTER),
+                    bgcolor="#ff6347"
+                )
+                e.page.snack_bar.duration = 3000
+                e.page.snack_bar.open = True
+                e.page.update()
+        else:
+            e.page.snack_bar = ft.SnackBar(
+                content=ft.Text("El campo de razón social está vacío", text_align=ft.TextAlign.CENTER),
+                bgcolor="#ff6347"
+            )
+            e.page.snack_bar.duration = 3000
+            e.page.snack_bar.open = True
+            e.page.update()
+
+
+    row_search_razon_social = ft.Row(
         controls=[
-            ft.TextField(
-                label="Razón social",
-                hint_text="Buscar transportista",
-                label_style=ft.TextStyle(color=ft.colors.LIGHT_BLUE_400),
-                border_width=0.7,
-                border_color=ft.colors.LIGHT_BLUE_400,
-                width=420
-            ),
+            razon_social,
             ft.IconButton(
                 icon="SEARCH",
                 icon_size=40,
-                icon_color=ft.colors.LIGHT_BLUE_400
+                icon_color=ft.colors.LIGHT_BLUE_400,
+                on_click=search_razon_social
             )
         ],
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN
@@ -44,7 +129,16 @@ def _view_():
             ]
         ),
         controls=[
-            row_search_transportista,
-            container_delete_button
+            row_search_razon_social,
+            correos,
+            ft.Container(
+                content=ft.ElevatedButton(
+                    text="Eliminar",
+                    icon="DELETE",
+                    on_click=delete_razon_social
+                ),
+                height=150,
+                alignment=ft.alignment.bottom_right
+            ),
         ]
     )
